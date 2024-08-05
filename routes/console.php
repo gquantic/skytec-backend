7d28eb4c80af442755392dc0d104bd23f7ad45e0
gquantic <darkkukri0550@gmail.com>
2024-08-05 20:05:07 +0300

    fix: users:update command every ten minutes

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('users:update', function () {
+
+})->everyTenMinutes();
+
+Artisan::command('cms:heads', function () {
+
+})->everyTenMinutes();
