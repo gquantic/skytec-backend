645797a6b8202a1e3d9897c12cfe6ce06bddc61d
k9lis <k9lis@yandex.ru>
2024-08-12 22:28:46 +0700

    В MenuItemsListLayout.php и LeftMenuItemsListLayout.php в методах delete добавлен параметр 'menuItem' => ->id

---
+++ app/Orchid/Layouts/MenuItem/LeftMenuItemsListLayout.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Orchid\Layouts\MenuItem;
+
+use App\Models\Menu\MenuItem;
+use Orchid\Screen\Actions\Button;
+use Orchid\Screen\Actions\Link;
+use Orchid\Screen\Actions\ModalToggle;
+use Orchid\Screen\Layouts\Table;
+use Orchid\Screen\TD;
+
+class LeftMenuItemsListLayout extends Table
+{
+    /**
+     * Data source.
+     *
+     * The name of the key to fetch it from the query.
+     * The results of which will be elements of the table.
+     *
+     * @var string
+     */
+    protected $target = 'leftMenuItems';
+
+    protected $title = 'Левое меню';
+
+    /**
+     * Get the table cells to be displayed.
+     *
+     * @return TD[]
+     */
+    protected function columns(): iterable
+    {
+        return [
+            TD::make('menuItem.id', '#')
+                ->render(fn (MenuItem $menuItem) => $menuItem->id),
+
+            TD::make('menuItem.title', 'Название')
+                ->render(fn (MenuItem $menuItem) => $menuItem->title),
+
+            TD::make('menuItem.sort_top', 'Сортировка')
+                ->width(100)
+                ->render(function (MenuItem $menuItem) {
+                    return ModalToggle::make(' ' . $menuItem->sort_left)
+                        ->modal('editLeftSortModal')
+                        ->modalTitle('Редактирование позиции ссылки в меню')
+                        ->asyncParameters([
+                            'menuItem' => $menuItem->id,
+                        ])
+                        ->method('editLeftSort')
+                        ->icon('pencil');
+                }),
+
+            TD::make('menuItem.edit', '')
+                ->width(100)
+                ->render(function (MenuItem $menuItem) {
+                    return Link::make('Редактировать')
+                        ->icon('pencil')
+                        ->route('platform.menu-items.edit', $menuItem->id);
+                }),
+
+            TD::make('menuItem.delete', '')
+                ->width(100)
+                ->render(function (MenuItem $menuItem) {
+                    return Button::make('Удалить')
+                        ->icon('trash')
+                        ->confirm('Вы уверены? Ссылка будет удалена из верхнего и левого меню. Если вы хотите убрать ссылку из определенного меню, то просто отредактируйте ссылку.')
+                        ->method('delete')
+                        ->parameters([
+                            'menuItem' => $menuItem->id,
+                        ]);
+                }),
+        ];
+    }
+}
